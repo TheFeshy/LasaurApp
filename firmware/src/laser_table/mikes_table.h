9ba38dbf0c0e1ee4924771b0cc506266e3a44cfc
TheFeshy <TheFeshy@gmail.com>
2016-04-18 11:53:41 -0400

    Added comment to explain an ambiguous setting

--- firmware/src/laser_table/mikes_table.h
+++ firmware/src/laser_table/mikes_table.h
@@ -7,7 +7,7 @@ This file is for configuring table-specifc values.
 #define CONFIG_Y_STEPS_PER_MM 78.6 //microsteps/mm (no integers, e.g. use 80.0 instead of 80)
 #define CONFIG_Z_STEPS_PER_MM 33.33333333 //microsteps/mm (no integers, e.g. use 80.0 instead of 80)
 
-#define CONFIG_PULSE_MICROSECONDS 5
+#define CONFIG_PULSE_MICROSECONDS 5 //Number of μs that a step pulse should last
 #define CONFIG_FEEDRATE 8000.0 // in millimeters per minute
 #define CONFIG_SEEKRATE 8000.0
 #define CONFIG_ACCELERATION 1800000.0 // mm/min^2, typically 1000000-8000000, divide by (60*60) to get mm/sec^2
